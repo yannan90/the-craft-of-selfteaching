ffe9560bafe2471658af8ac6107c5a4ec512be5c
Yannan <940364179@qq.com>
2019-12-23 23:45:51 -0500

    12/23/2019 study done

--- my-notes/my-notes.py
+++ my-notes/my-notes.py
@@ -1,1 +1,5 @@
-12/23/2019 test git function
+12/23/2019 
+test git function
+python toturial: https://docs.python.org/3/tutorial/index.html
+勾心斗角的套路历史上全都被反复用过了。倒是有本中文书值得吐血推荐，民国时代的作者连阔如先生写的《江湖丛谈》
+选择阅读“有繁殖能力”的内容
